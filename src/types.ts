f8d70db296d70fc81a2ef330bc8bbaa21f809b36
Moustapha HappyDev <moustapha.happydev@gmail.com>
2022-12-12 12:50:31 +0000

    remove irrelevant types

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,1 @@
-import type { APIContext } from 'astro';
-import type { AnyRouter, inferRouterContext } from '@trpc/server';
-
-export type CreateContextFn<TRouter extends AnyRouter> = (
-    APIContext: APIContext
-) => inferRouterContext<TRouter> | Promise<inferRouterContext<TRouter>>;
+export {};
